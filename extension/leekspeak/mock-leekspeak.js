5f7f0a2eac634066b6344f77072e48b60ca64d4d
David Huerta <huertanix@opentil.com>
2016-08-31 12:18:50 -0400

    Working regex and also green line for sanity check.

--- extension/leekspeak/mock-leekspeak.js
+++ extension/leekspeak/mock-leekspeak.js
@@ -1,5 +1,11 @@
-var onions = resultsPageSource.match(/^[a-zA-Z2-7]{16}\.onion$/g);
+var onions = document.documentElement.outerHTML.match(/[a-zA-Z2-7]{16}\.onion/);
 
-//for (var n = 0; n < onions.length; n++) {
-	console.log(onions);
-//}
+// Sanity check to make sure syntax is ok since Firefox add-ons just fail silently otherwise
+document.body.style.borderTop = "5px solid green";
+
+//alert(onions.length);
+console.log(onions);
+
+for (var n = 0; n < onions.length; n++) {
+	alert(onions[n]);
+}
