7140bd7702015b06a4c99e14ec92a79a44ae3578
David Huerta <huertanix@opentil.com>
2016-09-04 01:05:58 -0400

    Defining CSS attribes in JS to avoid css injection troubles.

--- extension/leekspeak/mock-leekspeak.js
+++ extension/leekspeak/mock-leekspeak.js
@@ -20,14 +20,26 @@ if (all_links !== null && all_links !== undefined) {
             if (onion_urls.length > 0) {
                 // <a> tag computed height
                 var a_tag_height = all_links[n].offsetHeight;
-
                 var ls_label = document.createElement("div");
-                ls_label.className = "ls-label";
+                // Add common style attrbutes directly into style tags; Using a css 
+                // class would introduce the possibility of name collisions with anything
+                // on the page that would share the class name.
+                //ls_label.className = "ls-label";
+                ls_label.style.color = "#FFF";
+                ls_label.style.background = "rgba(0, 0, 0, 0.75)";
+                ls_label.style.position = "absolute";
+                ls_label.style.fontFamily = "Helvetica, Arial, Sans";
+                ls_label.style.zIndex = 9000;
+                ls_label.style.border = "1px solid #800080";
+                ls_label.style.padding = "1px 5px";
+                
                 // Show label below the link
                 ls_label.style.top = all_links[n].offsetTop + a_tag_height;
                 // NOTE: Might be some i10n issues with setting left vs. right languages
                 ls_label.style.left = all_links[n].offsetLeft;
                 ls_label.innerHTML = "extensively red pear dean nils casket";
+
+                document.body.appendChild(ls_label);
             }
         }
     }    
